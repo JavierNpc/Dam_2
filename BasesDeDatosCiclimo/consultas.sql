55ce9eea859c700be190ab7a6df885470e9af171
JavierNpc <javiermacedacastro@gmail.com>
2024-09-13 13:16:49 +0200

    mod

--- BasesDeDatosCiclimo/consultas.sql
+++ BasesDeDatosCiclimo/consultas.sql
@@ -70,32 +70,47 @@ ORDER BY ciclista.nombre
 
 --14 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-SELECT  ciclista.nombre, etapa.netapa, color FROM maillot
-    INNER JOIN llevar on llevar.codigo = maillot.codigo
-    INNER JOIN etapa on etapa.netapa = llevar.netapa
-    INNER JOIN ciclista on ciclista.dorsal = llevar.dorsal
-    WHERE color = 'Amarillo' 
-GROUP BY ciclista.nombre , etapa.netapa ,color
+SELECT DISTINCT  ciclista.nombre, etapa.netapa FROM ciclista
+INNER JOIN etapa USING ( dorsal)
+INNER JOIN Llevar USING (dorsal)
+INNER JOIN maillot USING (codigo)
+    WHERE color = 'Amarillo' and llevar.codigo = maillot.codigo
+
 
 --15
 
-SELECT * FROM etapa 
-WHERE llegada != salida
-
-
- 
+SELECT e1.netapa from etapa e1 , etapa e2
+WHERE e1.netapa-1 = e2.netapa AND e1.salida <> e2.llegada
 
 
 
+--(Subconsultas)
+
+--16
 
 
+SELECT DISTINCT etapa.netapa, salida FROM etapa 
+WHERE netapa not in (SELECT netapa from puerto )
+               
+   
+   
+
+--20
+SELECT ciclista.dorsal, nombre FROM ciclista INNER JOIN puerto USING (dorsal)
+WHERE ciclista.dorsal = puerto.dorsal and altura = (SELECT max(altura) from puerto)
+
+--23
+
+SELECT dorsal, nombre FROM ciclista as c
+WHERE (SELECT count(*) from puerto WHERE c.dorsal = puerto.dorsal)>1
 
 
+--24
 
+SELECT DISTINCT p1.netapa from puerto p1
+WHERE not EXISTS (SELECT * from puerto p2 WHERE p2.altura <=700 and p1.netapa = p2.netapa)
 
-
-
-
+--25
 
 
 
