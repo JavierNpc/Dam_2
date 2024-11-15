0fc31630a648c1268a21aa424311c9f463e5183b
JavierNpc <javiermacedacastro@gmail.com>
2024-11-15 14:01:33 +0100

    mod

--- AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/userController.java
+++ AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/userController.java
@@ -1,12 +1,48 @@
 package com.example.springboot_web.Controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class userController {
         
+
+    
+    @GetMapping("/details")
+    public Map<Integer,HashMap> detailMap(){
+        Map<Integer,HashMap> body = new HashMap<>();
+        HashMap<String,String> usu = new HashMap();
+        ArrayList<String> list = new ArrayList();
+
+        for (int i = 0; i < 4; i++) {
+            
+            
+            usu.put("title", "Hola Mundo");
+            usu.put("name", "Paco");
+            usu.put("lastname", "Rest");
+
+            body.put(i, usu);
+        }
+
+        
+       
+    
+        return body;
+    }
+
+
+}
+
+/* 
+
     @GetMapping("/llamada")
     public String mostrar(Model model){
         model.addAttribute("title", "Hola mundo");
@@ -15,4 +51,17 @@ public class userController {
         return "details";
     }
 
-}
+
+@GetMapping("/details")
+    public Map<Integer,HashMap> detailMap(){
+        Map<Integer,HashMap> body = new HashMap<>();
+    
+        HashMap<String,Object> usu = new HashMap<>();
+        for(int i = 0 ; i<= 3 ; i++){
+            usu.put("name", "Javi"+i);
+            body.put(i,  usu);
+        }
+       
+        return body;
+    }
+         */
