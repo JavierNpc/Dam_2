c066653ad701d79bddef4a536bd65f14798c88b5
JavierNpc <javiermacedacastro@gmail.com>
2024-11-18 13:23:43 +0100

    mod

--- AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/userController.java
+++ AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/userController.java
@@ -1,43 +1,44 @@
 package com.example.springboot_web.Controllers;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.Map;
 
-
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot_web.Models.User;
 
-@RestController
+
+@Controller
 public class userController {
-        
 
-    
-    @GetMapping("/details")
-    public Map<Integer,HashMap> detailMap(){
-        Map<Integer,HashMap> body = new HashMap<>();
-        HashMap<String,String> usu = new HashMap();
-        ArrayList<String> list = new ArrayList();
-
-        for (int i = 0; i < 4; i++) {
-            
-            
-            usu.put("title", "Hola Mundo");
-            usu.put("name", "Paco");
-            usu.put("lastname", "Rest");
-
-            body.put(i, usu);
+    /* 
+        @GetMapping("/llamada")
+        public String mostrar(Model model){
+            model.addAttribute("title", "Hola mundo");
+            model.addAttribute("name", "Javi");
+            model.addAttribute("lastname", "Maceda");
+            return "details";
         }
+    */
+        
+    @GetMapping("/models")
+    public String mostrar(Map<String,Object> model){
+       
+      User user = new User("Mjose", "Martin");
 
+        model.put("title", "hola mundo");
+        model.put("user", user);
         
        
-    
-        return body;
-    }
 
+       
+    
+        return "details";
+    }
 
 }
 
@@ -52,16 +53,4 @@ public class userController {
     }
 
 
-@GetMapping("/details")
-    public Map<Integer,HashMap> detailMap(){
-        Map<Integer,HashMap> body = new HashMap<>();
-    
-        HashMap<String,Object> usu = new HashMap<>();
-        for(int i = 0 ; i<= 3 ; i++){
-            usu.put("name", "Javi"+i);
-            body.put(i,  usu);
-        }
-       
-        return body;
-    }
-         */
+*/
