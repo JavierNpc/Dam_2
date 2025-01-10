6375641bd60304804b1fd8d8fe52883974c1bcae
JavierNpc <javiermacedacastro@gmail.com>
2025-01-10 10:47:07 +0100

    A casa

--- Desrrollo_Angular/PrimerProyecto/src/app/app.component.ts
+++ Desrrollo_Angular/PrimerProyecto/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Hello world!</h1>
-  `,
+  imports: [HomeComponent,RouterModule ],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- Desrrollo_Angular/PrimerProyecto/src/app/home/home.component.ts
+++ Desrrollo_Angular/PrimerProyecto/src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  
+  housingLocationList: HousingLocation[] = [];
 
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
